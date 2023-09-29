2c5fc5b51cd233f115f48b846e3419e3cbb8a9c3
flodro34 <f.derosso@gmail.com>
2023-09-29 10:00:02 +0200

    test-commit

---
+++ src/main/java/com/flo/App.java
@@ -0,0 +1,14 @@
+package com.flo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
